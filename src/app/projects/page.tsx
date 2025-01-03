769b42d4173c2894a3cc34446074fb6083481898
vishwas0612 <IIT2024087@iiita.ac.in>
2025-01-03 20:22:41 +0530

    fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,18 +1,18 @@
-import InfoMatrix from "@/components/self/infomatrics";
-import ProJects from "@/components/self/ProJects";
+// import InfoMatrix from "@/components/self/infomatrics";
+// import ProJects from "@/components/self/ProJects";
 
-export default function Projects(){
-   return(
-   <div className="w-full h-full absolute inset-0 bg-gradient-to-b from-orange-800 via-red-900 to-black">
-      <div className="w-full h-full absolute inset-0">
-          <ProJects></ProJects>
-      </div>
-      <div>
-         <InfoMatrix></InfoMatrix>
-         <div>
-            hlo 2.0
-         </div>
-      </div>
-      </div>
-   )
-} 
+// export default function Projects(){
+//    return(
+//    <div className="w-full h-full absolute inset-0 bg-gradient-to-b from-orange-800 via-red-900 to-black">
+//       <div className="w-full h-full absolute inset-0">
+//           <ProJects></ProJects>
+//       </div>
+//       <div>
+//          <InfoMatrix></InfoMatrix>
+//          <div>
+//             hlo 2.0
+//          </div>
+//       </div>
+//       </div>
+//    )
+// } 
